944be9006d49b1805f604d50dab92b085e22ad6f
leesol <jbkljo@naver.com>
2022-05-12 21:14:03 +0900

    메인 타이틀 이미지, 애니메이션 추가

--- js/main.js
+++ js/main.js
@@ -35,3 +35,11 @@ window.addEventListener('scroll', _.throttle(function(){
   }
 },300)); //300은 0.3초 함수가 한번에 막 실행되는 걸 방지!(throttle)
 // _.throttle(함수,시간(ms))
+
+const fadeEls =document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl,index){
+  gsap.to(fadeEl,1,{
+    delay:(index+1)*.7,
+    opacity: 1
+  });
+});
