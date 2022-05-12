72cce8ee19903261db535e895f7ec6cc708220db
leesol <jbkljo@naver.com>
2022-05-12 17:18:20 +0900

    badge(배지)추가

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ searchInputEl.addEventListener('blur',function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder',''); //(속성이름,속성에 들어갈 실제 값)
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if(window.scrollY >500){
+    //배지숨기기
+    //gsap.to(요소,지속시간(s),옵션);
+    gsap.to(badgeEl, .6, {
+      opacity:0,
+      display: 'none'
+    });
+  }else{
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity:1,
+      display: 'block'
+    });
+  }
+},300)); //300은 0.3초 함수가 한번에 막 실행되는 걸 방지!(throttle)
+// _.throttle(함수,시간(ms))
