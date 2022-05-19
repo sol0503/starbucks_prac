0eb9630ec42e8bfa58279b39b9cfe6e255a88e86
leesol <jbkljo@naver.com>
2022-05-20 01:16:42 +0900

    푸터 추가

--- js/main.js
+++ js/main.js
@@ -129,3 +129,6 @@ spyEls.forEach(function(spyEl){
     .setClassToggle(spyEl,'show')
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear=document.querySelector('.this-year');
+thisYear.textContent=new Date().getFullYear(); //현재 연도 표시
